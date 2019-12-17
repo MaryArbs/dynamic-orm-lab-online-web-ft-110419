a615d71817e5091b112bd024c4357d07abe57892
Mary Arbachauskas <mary.arbachauskas@gmail.com>
2019-12-17 15:29:10 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -1,9 +1,9 @@
 require_relative "../config/environment.rb"
-require 'active_support/inflector' #alloes pluralize 
+require 'active_support/inflector' #alloes pluralize
 
 class InteractiveRecord
 
   def self.table_name
     self.to_s.downcase.pluralize
   end
-end 
+end
